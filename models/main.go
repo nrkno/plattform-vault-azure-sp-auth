8581b6007db030e9aa5612be6ac731acf263a523
Martin Bruset Solberg <martin.bruset.solberg@nrk.no>
2023-12-04 09:35:41 +0100

    fix: combine redundant files in models package

--- models/main.go
+++ models/main.go
@@ -1,1 +1,11 @@
 package models
+
+type AzureConfig struct {
+	ServicePrincipalPath string `json:"SERVICE_PRINCIPAL_PATH"`
+	TenantID             string `json:"TENANT_ID"`
+}
+
+type AzureCredentials struct {
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
