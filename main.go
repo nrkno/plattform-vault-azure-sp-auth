6206cba6f5fa9904af290fb16eada6822c142a44
Martin Bruset Solberg <martin.bruset.solberg@nrk.no>
2023-12-06 15:29:46 +0100

    fix: instansialize the vaultCfg var

--- main.go
+++ main.go
@@ -14,7 +14,10 @@ func GetCreds(VAULT_ADDR string, VAULT_AZURE_CONFIG_PATH string) (*models.AzureC
 		VAULT_AZURE_CONFIG_PATH = "default_vault_azure_config_path"
 	}
 	// Vault Azure SP
-	var vaultCfg models.VaultConfig
+	var vaultCfg models.VaultConfig = models.VaultConfig{
+		VaultAddress:         VAULT_ADDR,
+		VaultAzureConfigPath: VAULT_AZURE_CONFIG_PATH,
+	}
 	spCred, err := config.ReadVaultPath[models.AzureCredentials](vaultCfg, nil)
 	if err != nil {
 		return nil, err
